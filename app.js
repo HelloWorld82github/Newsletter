6724df77de36a337440388e1b851fd9c878d8736
Mridula Rao <hellogit82@gmail.com>
2021-07-02 11:01:54 +0530

    second commit page is a lil modified

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require("express");
+const client = require("@mailchimp/mailchimp_marketing");
 const app = express();
 
 app.use(express.urlencoded());
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
-app.get("/", function(req,res){
-    res.sendFile(__dirname + "/index2.html");
-})
+
+app.get("/", function(req, res){
+    res.render('titlepage', {
+        titleOfToday : 'Hasan Minaj: Comedian 2.0'
+    });
+
+});
 
 
 app.listen(3000, function(req,res){
